8fbd0cc4bcdea020932dac61eb135d24ddc92344
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-06-13 17:13:21 -0400

    Second iteration on exercise 2

--- workshop/exercise-2/exercise-2-1/app.js
+++ workshop/exercise-2/exercise-2-1/app.js
@@ -1,66 +1,70 @@
-const TRACK = document.querySelector('#track');
-const HEAD = document.querySelector('head');
-const FROGS = 3;
+class FrogRace {
+  constructor() {
+    this.track = document.querySelector('#track');
+    this.trackLength = track.offsetWidth;
+    this.racingFrogs = Array.from(frogStable);
+    this.numberOfRacers = 3;
+    this.ranking = [];
+  }
 
-let styles = document.createElement('link');
-styles.setAttribute('rel', 'stylesheet');
-styles.setAttribute('href', '../assets/styles.css');
-HEAD.appendChild(styles);
-
-let ranking = [];
-
-function racingFrog(frog) {
-  let progress = frog.progress;
-  const trackWidth = track.offsetWidth;
-  const distanceHopped = (Math.floor(Math.random() * 100) / trackWidth) * 100;
-
-  const bounce = setInterval(() => {
-    progress += distanceHopped;
-
-    if (progress > 100) {
-      progress = 100;
-      console.log(frog.name, ' has finished!');
-      clearInterval(bounce);
-
-      ranking.push(frog);
+  determineRacers() {
+    while (this.racingFrogs.length > this.numberOfRacers) {
+      let index = Math.floor(Math.random() * this.racingFrogs.length);
+      this.racingFrogs.splice(index, 1);
     }
+    return self;
+  }
 
-    document.querySelector(`#${frog.lane} .frog`).style.left = `${progress}%`;
-  }, Math.random() * 1000);
-}
+  createTrack() {
+    for (let i = 0; i < this.numberOfRacers; i++) {
+      let lane = document.createElement('li');
+      let laneNumber = document.createElement('span');
+      let frog = document.createElement('span');
+      let frogProgress = document.createElement('span');
 
-racingFrogs = Array.from(frogStable);
+      this.racingFrogs[i].progress = 0;
+      this.racingFrogs[i].lane = `frog-${i}`;
 
-function determineRacers() {
-  while (racingFrogs.length > FROGS) {
-    let index = Math.floor(Math.random() * racingFrogs.length);
-    racingFrogs.splice(index, 1);
+      lane.setAttribute('id', `frog-${i}`);
+      laneNumber.textContent = i + 1;
+      frog.textContent = this.racingFrogs[i].number;
+      frog.setAttribute('class', 'frog');
+      frog.style.backgroundColor = this.racingFrogs[i].color;
+      frogProgress.setAttribute('id', this.racingFrogs[i].name);
+
+      this.track.appendChild(lane);
+      lane.appendChild(laneNumber);
+      lane.appendChild(frog);
+      lane.appendChild(frogProgress);
+    }
+    return self;
+  }
+
+  startRace() {
+    this.racingFrogs.forEach(frog => this.runRace(frog));
+  }
+
+  runRace(frog) {
+    let progress = frog.progress;
+    const distanceHopped = (Math.floor(Math.random() * 100) / this.trackLength) * 100;
+
+    const bounce = setInterval(() => {
+      progress += distanceHopped;
+
+      if (progress > 100) {
+        progress = 100;
+        console.log(frog.name, ' has finished!');
+        clearInterval(bounce);
+
+        this.ranking.push(frog);
+      }
+
+      document.querySelector(`#${frog.lane} .frog`).style.left = `${progress}%`;
+    }, Math.random() * 1000);
   }
 }
 
-determineRacers();
-console.log(racingFrogs)
-for (let i = 0; i < FROGS; i++) {
-  console.log(i)
-  let lane = document.createElement('li');
-  let laneNumber = document.createElement('span');
-  let frog = document.createElement('span');
-  let frogProgress = document.createElement('span');
-
-  racingFrogs[i].progress = 0;
-  racingFrogs[i].lane = `frog-${i}`;
-
-  lane.setAttribute('id', `frog-${i}`);
-  laneNumber.textContent = i + 1;
-  frog.textContent = racingFrogs[i].number;
-  frog.setAttribute('class', 'frog');
-  frog.style.backgroundColor = racingFrogs[i].color;
-  frogProgress.setAttribute('id', racingFrogs[i].name);
-
-  TRACK.appendChild(lane);
-  lane.appendChild(laneNumber);
-  lane.appendChild(frog);
-  lane.appendChild(frogProgress);
-
-  racingFrog(racingFrogs[i])
-}
+let frogRace = new FrogRace();
+frogRace.determineRacers();
+frogRace.createTrack();
+frogRace.startRace();
