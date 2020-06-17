ed9a84807f693968736b2037173aab9e6e7e6460
Andrew Baldwin <andrew.baldwin44@gmail.com>
2020-06-17 12:47:48 -0400

    Exercise 2: Play multiple races

--- workshop/exercise-2/assets/scripts/app.js
+++ workshop/exercise-2/assets/scripts/app.js
@@ -2,9 +2,13 @@ class FrogRace {
   constructor() {
     this.track = document.querySelector('#track');
     this.trackLength = track.offsetWidth;
-    this.racingFrogs = Array.from(frogStable);
+    this.racingFrogs = this.cloneFrogStable();
     this.numberOfRacers = 3;
     this.ranking = [];
+  }
+
+  cloneFrogStable() {
+    return Array.from(frogStable);
   }
 
   determineRacers() {
@@ -20,6 +24,19 @@ class FrogRace {
     for (let i = 0; i < this.numberOfRacers; i++) {
       let lane = document.createElement('li');
       let laneNumber = document.createElement('span');
+
+      lane.setAttribute('id', `frog-${i}`);
+      laneNumber.textContent = i + 1;
+
+      this.track.appendChild(lane);
+      lane.appendChild(laneNumber);
+    }
+    return this;
+  }
+
+  createFrogs() {
+    for (let i = 0; i < this.numberOfRacers; i++) {
+      let lane = document.querySelector(`#frog-${i}`);
       let frog = document.createElement('img');
       let frogProgress = document.createElement('span');
 
@@ -27,30 +44,26 @@ class FrogRace {
       this.racingFrogs[i].progress = 0;
       this.racingFrogs[i].lane = `frog-${i}`;
 
-      lane.setAttribute('id', `frog-${i}`);
-      laneNumber.textContent = i + 1;
-
-      console.log(this.racingFrogs[i].color);
-
       frog.setAttribute('src', `assets/images/${this.racingFrogs[i].color}.png`);
       frog.setAttribute('alt', this.racingFrogs[i].number);
       frog.setAttribute('class', 'frog');
       frogProgress.setAttribute('id', this.racingFrogs[i].name);
 
-      this.track.appendChild(lane);
-      lane.appendChild(laneNumber);
       lane.appendChild(frog);
       lane.appendChild(frogProgress);
     }
+
     return this;
   }
 
   startRace() {
+    newRace.disabled = true;
     this.racingFrogs.forEach(frog => this.runRace(frog));
   }
 
   runRace(frog) {
     let progress = frog.progress;
+    let currentProgress = document.querySelector(`#${frog.lane} .frog`);
     const distanceHopped = (Math.floor(Math.random() * 100) / this.trackLength) * 100;
 
     let bounce = setInterval(() => {
@@ -60,13 +73,12 @@ class FrogRace {
         progress = 100;
 
         this.ranking.push(frog);
-        console.log(frog.name, ' has finished!');
         this.placePodium(frog);
 
         clearInterval(bounce);
       }
       else {
-        document.querySelector(`#${frog.lane} .frog`).style.left = `${progress}%`;
+        currentProgress.style.left = `${progress}%`;
       }
     }, Math.random() * 1000);
   }
@@ -75,11 +87,9 @@ class FrogRace {
     frog.frog.style.transform = 'scale(0.1)';
 
     let rank = this.ranking.indexOf(frog);
-    let podiumSection = document.querySelector('.podium');
     let stand = document.querySelector(`#frog-seat-${rank + 1}`);
     let numberPlace = document.querySelector(`#frog-number-${rank + 1}`)
     let namePlace = document.querySelector(`#frog-name-${rank + 1}`);
-    stand.style.transition = 'transform 2s';
 
     setTimeout(() => {
       frog.frog.style.transform = 'scale(1)';
@@ -87,19 +97,39 @@ class FrogRace {
       stand.appendChild(frog.frog);
       numberPlace.textContent = `#${frog.number}`;
       namePlace.textContent = frog.name;
+
+      if (this.ranking.length == this.numberOfRacers) newRace.disabled = false;
     }, 2000);
   }
 
-  restartRace() {
-    clearInterval(bounce);
-    this.startRace()
+  removeTextContent(elements) {
+    [...elements].forEach(element => element.textContent = '');
+  }
+
+  removePreviousRacers() {
+    let frogSeats = document.querySelectorAll('.frog-seat');
+    let frogNumbers = document.querySelectorAll('.frog-number');
+    let frogNames = document.querySelectorAll('.frog-name');
+
+    [...frogSeats].forEach(frogSeat => frogSeat.firstChild.remove());
+    this.removeTextContent(frogNumbers);
+    this.removeTextContent(frogNames);
+  }
+
+  newRace() {
+    this.removePreviousRacers();
+    this.ranking = [];
+    this.racingFrogs = this.cloneFrogStable();
+    this.determineRacers();
+    this.createFrogs();
+    this.startRace();
   }
 }
 
-let frogRace = new FrogRace();
-frogRace.determineRacers().createTrack().startRace();
-
-newRace = document.querySelector('#new-race');
+let newRace = document.querySelector('#new-race');
 newRace.addEventListener('click', () => {
-  // frogRace.restartRace();
+  frogRace.newRace();
 });
+
+let frogRace = new FrogRace();
+frogRace.determineRacers().createTrack().createFrogs().startRace();
